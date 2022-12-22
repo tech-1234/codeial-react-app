e484b2f3942d5d11b5f3ace37a6285ae36c3e825
tech-1234 <ankitdutta1741@gmail.com>
2022-12-22 23:41:33 +0530

    Styling Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,30 @@
+import styles from '../styles/home.module.css';
+
 const Home = () => {
   return (
-    <div className="posts-list">
-      <div className="post-wrapper">
-        <div className="post-header">
-          <div className="post-avatar">
+    <div className={styles.postList}>
+      <div className={styles.postWrapper}>
+        <div className={styles.postHeader}>
+          <div className={styles.postAvatar}>
             <img
               src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
               alt="user-pic"
             />
             <div>
-              <span className="post-author">Ankit</span>
-              <span className="post-time">a minute ago</span>
+              <span className={styles.postAuthor}>Ankit</span>
+              <span className={styles.postTime}>a minute ago</span>
             </div>
           </div>
-          <div className="post-content">Post Content</div>
-          <div className="post-actions">
-            <div className="post-like">
+          <div className={styles.postConten}>Post Content</div>
+          <div className={styles.postActions}>
+            <div className={styles.postLike}>
               <img
                 src="https://cdn-icons-png.flaticon.com/512/889/889140.png"
                 alt="likes-icon"
               />
               <span>5</span>
             </div>
-            <div className="post-comments-icon">
+            <div className={styles.postCommentsIcon}>
               <img
                 src="https://cdn-icons-png.flaticon.com/512/2190/2190552.png"
                 alt="comments-icon"
@@ -30,17 +32,17 @@ const Home = () => {
               <span>2</span>
             </div>
           </div>
-          <div className="post-comment-box">
-            <input placeholer="Start typing a comment" />
+          <div className={styles.postCommentBox}>
+            <input placeholder="Start typing a comment" />
           </div>
-          <div class="post-comments-list">
-            <div className="post-comments-item">
-              <div className="post-comment-header">
-                <span className="post-comment-author">Bill</span>
-                <span className="post-comment-time">a minute ago</span>
-                <span className="post-comment-likes">22</span>
+          <div class={styles.postCommentsList}>
+            <div className={styles.postCommnetsItem}>
+              <div className={styles.postCommentHeader}>
+                <span className={styles.postCommentAuthor}>Bill</span>
+                <span className={styles.postCommentTime}>a minute ago</span>
+                <span className={styles.postCommentLikes}>22</span>
               </div>
-              <div className="post-comment-content">Random comment</div>
+              <div className={styles.postCommentContent}>Random comment</div>
             </div>
           </div>
         </div>
