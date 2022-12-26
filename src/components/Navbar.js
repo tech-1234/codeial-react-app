1a106b5193e4c2ce0df66910370968a3431deb9a
tech-1234 <ankitdutta1741@gmail.com>
2022-12-27 03:59:05 +0530

    search result UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/navbar.module.css';
 import { useAuth } from '../hooks';
 
 const Navbar = () => {
   const auth = useAuth();
+  const [results, setResults] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
   return (
     <div className={styles.nav}>
@@ -14,6 +17,35 @@ const Navbar = () => {
             src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
           />
         </Link>
+      </div>
+      <div className={styles.searchContainer}>
+        <img
+          className={styles.searchIcon}
+          src="https://cdn-icons-png.flaticon.com/512/954/954591.png"
+          alt=""
+        />
+        <input
+          placeholder="Search users"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        {results.length > 0 && (
+          <div className={styles.searchResults}>
+            <ul>
+              {results.map((user) => (
+                <li className={styles.searchResultRow} key={`user-${user._id}`}>
+                  <Link to={`/users/${user._id}`}>
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/560/560277.png"
+                      alt=""
+                    />
+                    <span>{user.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className={styles.rightNav}>
         {auth.user && (
